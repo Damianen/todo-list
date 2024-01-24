793259715d55528af64b2ebe5a9801e0c9d0deff
Damianen <113175544+Damianen@users.noreply.github.com>
2024-01-24 13:19:12 +0100

    Add style to add task and weekly tasks

--- src/homePage.js
+++ src/homePage.js
@@ -38,15 +38,21 @@ export default function build() {
 
     const newTaskName = document.createElement("input");
     const newTaskPriority = document.createElement("select");
+    newTaskPriority.innerHTML = '<option value="">priority</option>';
     const newTaskBtn = document.createElement("button");
-    newTaskForm.append(newTaskName, newTaskPriority, newTaskBtn);
+    newTaskForm.append(newTaskBtn, newTaskName, newTaskPriority);
     newTaskBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24"><path d="M17,11H13V7a1,1,0,0,0-1-1h0a1,1,0,0,0-1,1v4H7a1,1,0,0,0-1,1H6a1,1,0,0,0,1,1h4v4a1,1,0,0,0,1,1h0a1,1,0,0,0,1-1V13h4a1,1,0,0,0,1-1h0A1,1,0,0,0,17,11Z"/></svg>'
 
     todaysTasks.append(text);
 
     todaysTasks.append(newTask);
 
-    main.append(todaysTasks);
+    const weekTasks = document.createElement("div");
+    const weekTasksTitle = document.createElement("h1");
+    weekTasksTitle.textContent = "Task due this week:";
+    weekTasks.append(weekTasksTitle);
+
+    main.append(todaysTasks, weekTasks);
 
     document.body.append(sideBar, main);
 }
